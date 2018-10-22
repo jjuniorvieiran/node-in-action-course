7c12d707480bf5fbce61a94e80b8973e2f8c535d
juniorhc <junior.jaava@gmail.com>
2018-10-22 19:21:13 +0100

    Working with database, npm + database connection

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,19 @@
 
 module.exports = function(app){
     app.get('/produtos', function(req,res){
-        res.render("produtos/lista");
+
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'root',
+            database : 'casadocodigo_nodejs'
+        });
+        connection.query('select * from livros', function(err, results){
+            res.send(results);
+        });
+
+        connection.end();
+
     });
 }

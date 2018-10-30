2180a0cebe6ee3c70bd9c300e0f4006a5869d64c
Júnior <junior.jaava@gmail.com>
2018-10-30 23:38:54 +0000

    isolating the connection

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,45 +1,17 @@
 
-
+var dbConnection = require('../infra/connectionFactory');
 
 module.exports = function(app){
     app.get('/produtos', function(req,res){
 
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : 'password',
-            database : 'casadocodigo_nodejs'
-        });
-        connection.query('select * from livros', function(err, results){
-            res.render('produtos/lista', {lista:results});
-            //res.send(err);
-            //res.send(results);
-        });
+    var connection = dbConnection();
 
-        connection.end();
+      connection.query('select * from livros',function(err,results){
+          res.render('produtos/lista', {lista:results});
+          //res.send(results);
+      });
+
+      connection.end();
 
     });
 }
-
-
-/*
-module.exports = function(app){
-    app.get('/produtos',function(req,res)){
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : '',
-            database : 'casadocodigo_nodejs'
-        });
-
-        connection.query('select * from livros',function(err,results){
-            res.send(results);
-
-        });
-
-        connection.end();
-    };
-}
-*/

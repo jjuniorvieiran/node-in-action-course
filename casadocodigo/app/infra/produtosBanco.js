2b87643cfa5514f63b3f3b592cc090ee9ec9b9e0
Júnior <junior.jaava@gmail.com>
2018-10-31 21:13:32 +0000

    receving the connection like parameter, giving state to object produtosBanco

--- casadocodigo/app/infra/produtosBanco.js
+++ casadocodigo/app/infra/produtosBanco.js
@@ -1,6 +1,12 @@
-module.exports = function(){
-        this.lista = function(connection, callback){
+module.exports = function(){ //trick to enganar load-express
+
+    return function(connection) {
+
+        this.lista = function(callback){
             connection.query('select * from livros', callback);
-            }
-            return this;
+        }
+
+        return this;
+    }
+
 }
--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,12 +1,16 @@
 module.exports = function(app){
+
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco;
+        var produtosBanco = app.infra.produtosBanco(connection); //paremeter to the object productBanco
 
-      produtosBanco.lista(connection, function(erros,resultados){
+        produtosBanco.lista(function(erros,resultados){ // paremeter to function lista
             res.render('produtos/lista',{lista:resultados});
+            });
 
-       });
+
         connection.end();
+
     });
+
 }
